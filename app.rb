b95067acb8b48163a8552c7546608c9a90d0f1a1
ts-miller <tylerstephen11@gmail.com>
2020-09-25 09:31:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Tyler"
+    end
+
+    get '/hometown' do
+        "My hometown is Baltic"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is September"
+    end
 end
